7051578326205349d42eb8e5ffa7048c5b06b0c5
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-03-24 14:27:51 +0200

    implemented sidebar

--- frontend/src/pages/Editor/Editor.js
+++ frontend/src/pages/Editor/Editor.js
@@ -9,7 +9,14 @@ export default function Editor() {
     const [signed_in, setSignedIn] = useState(false)
     const [user_photo, setUserPhoto] = useState()
     const [console_open, setConsoleOpen] = useState(false)
+    const [sidebar_open, setSidebarOpen] = useState(false)
+    const [sidebar_selected, setSidebarSelected] = useState('')
 
+    const openSideBar = (selected) => {
+        selected == sidebar_selected && setSidebarOpen(!sidebar_open)
+        setSidebarSelected(selected)
+    }
+    
     return (
         <div className='container'>
             <div className='header'>
@@ -18,9 +25,20 @@ export default function Editor() {
             </div>
             <div className='editor-container'>
                 <div className='side-bar'>
-                    <img src={search} className="menu-button bright"/>
-                    <img src={files} className="menu-button bright"/>
-                    <img src={messages} className="menu-button"/>
+                    <img src={search} className="menu-button bright" onClick={() => openSideBar('search')}/>
+                    <img src={files} className="menu-button bright" onClick={() => openSideBar('files')}/>
+                    <img src={messages} className="menu-button" onClick={() => openSideBar('messages')}/>
+                </div>
+                <div className={sidebar_open ? 'sidebar-open' : 'sidebar-closed'}>
+                    {sidebar_selected == 'search' && 
+                    <div>Search</div>
+                    }
+                    {sidebar_selected == 'files' && 
+                    <div>Files</div>
+                    }
+                    {sidebar_selected == 'messages' && 
+                    <div>Messages</div>
+                    }
                 </div>
                 <div className='editor'>
                     <textarea className='editor-input' placeholder='Write your code here...'></textarea>
