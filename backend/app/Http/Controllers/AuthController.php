d9f096750789eac39c550cd1ba2bf79eeae836df
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-03-25 19:47:21 +0200

    resolved conflicts

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -89,4 +91,18 @@ class AuthController extends Controller
         ]);
     }
 
+}
+<?php
+
+namespace App\Http\Controllers;
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //
 }
