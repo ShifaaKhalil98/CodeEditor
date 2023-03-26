701dd5983f6ee91d3fcff2d019a824ce48590ba3
MoeShoumar <mks38@mail.aub.edu>
2023-03-26 15:30:14 +0300

    navbar component with style

---
+++ frontend/src/components/navbar/index.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const Navbar = () => {
+  return (
+    <div className="header">
+      <h1>CODING</h1>
+      <span>Sign out</span>
+    </div>
+  );
+};
+
+export default Navbar;
