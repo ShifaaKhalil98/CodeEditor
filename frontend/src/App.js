46a8355de3a427a1eaa8d1c17500de279af2ba2e
SalmanMoussaa <Salman.m.moussa@gmail.com>
2023-03-26 17:51:22 +0300

    adding pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Editor from "./pages/Editor/Editor";
 import UserList from "./pages/Admin";
 import Profile from "./pages/profile";
 import ChatList from "./pages/Chat";
+import Login_Register from "./pages/Login_Register";
 
 import React from "react";
 // import ReactDOM from "react-dom";
@@ -18,6 +19,8 @@ function App() {
       <Route path="/profile" element={<Profile />} />
       <Route path="/admin" element={<UserList />} />
       <Route path="/chats" element={<ChatList />} />
+      <Route path="/Login_Register" element={<Login_Register />} />
+
 
       {/* <Route path="*" element={<NotFound />} /> */}
     </Routes>
