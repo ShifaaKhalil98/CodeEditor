01422eac1cf9de9ab485f6194e081e51d72b4945
SalmanMoussaa <Salman.m.moussa@gmail.com>
2023-03-26 20:26:57 +0300

    adding login forms

--- frontend/src/pages/Login_Register/index.js
+++ frontend/src/pages/Login_Register/index.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./index.css";
+import Login from "../../components/Login";
 export default function Login_Register(){
     return(
         <div className="flex">
           
-           
+           <Login/>
             
         </div>
     );
