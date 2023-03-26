e08d185eb1fcf4c6dab404f521b106931c4a54a2
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-03-26 14:56:24 +0300

    fixed a bug with UserCard component

--- frontend/src/components/UserCard/UserCard.js
+++ frontend/src/components/UserCard/UserCard.js
@@ -4,14 +4,12 @@ import profile_pic from '../../images/profile-pic.png'
 import bubble from '../../images/bubble.png'
 
 const UserCard = ({ name, pic=null }) => {
-  const [fullName, setfullName] = useState(name);
-  const [profilePic, setProfilePic] = useState(pic);
 
   return (
     <div className="card-container">
         <div className="details">
-            <img src={profilePic ? profilePic :profile_pic}/>
-            <h2>{fullName}</h2>
+            <img src={pic ? pic :profile_pic}/>
+            <h2>{name}</h2>
             <img className="msg" src={bubble}/>
         </div>
     </div>
