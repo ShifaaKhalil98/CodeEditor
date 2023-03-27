58fc00673e087d8f9e1eb57c26e28f4ba436c5f1
MoeShoumar <mks38@mail.aub.edu>
2023-03-27 09:42:49 +0300

    created function for image upload

--- backend/app/Http/Controllers/UserDataController.php
+++ backend/app/Http/Controllers/UserDataController.php
@@ -38,5 +38,16 @@ class UserDataController extends Controller{
         $file ->delete();
         return response()->json(['message'=>'File deleted succefully']);
     }
-  
+    function uploadImage(Request $request, $id){
+        $encoded = $request->encoded;
+        $decoded = base64_decode($encoded);
+    
+        $file_path = public_path('images/'. $id . 'op' . '.png');
+    
+        file_put_contents($file_path,$decoded);
+    
+        User::where("id",$id)->update("profile_picture", "http://localhost/images" . $id . ".png");
+        
+        return response()->json(['message'=>'success']);
+    }
 }
