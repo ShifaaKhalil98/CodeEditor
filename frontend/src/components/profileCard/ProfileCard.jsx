c8b856e3623cc8a94b807f27b7524fc1f74c72be
MoeShoumar <mks38@mail.aub.edu>
2023-03-27 20:47:09 +0300

    fixed some typos

--- frontend/src/components/profileCard/ProfileCard.jsx
+++ frontend/src/components/profileCard/ProfileCard.jsx
@@ -1,15 +1,15 @@
 // import "./style.css";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "../../../src/base.css";
+// import "../../../src/base.css";
 import profile_pic from "../../images/profile_pic.png";
-
+import "./index.css";
 const ProfileCard = ({ name, pic = null }) => {
   // const [fullName, setfullName] = useState(name);
   // const [profilePic, setProfilePic] = useState(pic);
 
   return (
-    <div className="profile-card flex fd-column jc-center ">
+    <div className="profile-card ">
       <div className="image">
         <img src={pic ? pic : profile_pic} />
       </div>
--- frontend/src/pages/profile/profile.jsx
+++ frontend/src/pages/profile/profile.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Navbar from "../../components/Navbar";
+import Navbar from "../../components/navbar/navbar";
 import ProfileCard from "../../components/profileCard/ProfileCard";
 import File from "../../components/filediv";
 import "../../../src/base.css";
@@ -62,16 +62,32 @@ const Profile = () => {
       });
   };
   // didndt test the reponse data
-
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setImageData(reader.result);
+      axios
+        .post(`${baseUrl}/api/uploadImage`, { imageData: reader.result })
+        .then((response) => {
+          setImageData(response.data.image_url);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    reader.readAsDataURL(file);
+  };
   return (
     <div>
       <div className="nav-main">
         <Navbar />
       </div>
       <div className="main flex jc-center ai-cneter ">
-        <div className="profile-main fd-column flex ai-fs jc-center">
+        <div className="profile-main">
           <ProfileCard name={"ayman"} pic={imageData} />
-          <div>
+          <div className="input-container">
+            <h3>Update Profile Pic</h3>
             <input type="file" onChange={handleImageChange} />
           </div>
         </div>
