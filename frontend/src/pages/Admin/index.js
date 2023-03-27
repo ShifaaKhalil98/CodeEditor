0b59969c8d22989ff277e11609feebe6a484286f
SalmanMoussaa <Salman.m.moussa@gmail.com>
2023-03-27 17:48:11 +0300

    linking ti register api

--- frontend/src/pages/Admin/index.js
+++ frontend/src/pages/Admin/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./Admin.css";
-import Navbar from "../../components/navbar";
+import Navbar from "../../components/Navbar";
 
 function UserList() {
   const [users, setUsers] = useState([]);
